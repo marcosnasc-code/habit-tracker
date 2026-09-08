707a790f5180028b9d442f0454c0aae5236daff8
unknown <marcos.nacimento0321@gmail.com>
2026-09-08 10:41:51 -0300

    Criação de tabelas habits e habits_log.

--- database/migrations/2026_09_08_131607_create_habits_table.php
+++ database/migrations/2026_09_08_131607_create_habits_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('habits', function (Blueprint $table) {
             $table->id();
+            $table
+                ->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('name')
+                ->nullable()
+                ->unique();
             $table->timestamps();
         });
     }
